99273a25c920f4670c07bb6f8943543b27775c6e
kbriggs <kabriggs991@gmail.com>
2017-11-12 21:47:27 -0500

    better jQuery solution

--- js/picViewerJS.js
+++ js/picViewerJS.js
@@ -6,34 +6,29 @@ var pics = $('img');
 var originalHeight = pics[0].height;
 var originalWidth = pics[0].width;
 
-// Adds a mouseover event listner to expand the height and width
-$('img').mouseover( function () {
-    $(this).height(100).width(120);
-}); // END mouseover listener
+// Add listeners for all the img tags. Notice we can chain them
+pics
+    .mouseover( function () {
+        $(this).height(100).width(120);
+    })
+    .mouseout( function () {
+        $(this).height(originalHeight).width(originalWidth);
+    })
+    .click( function (event) {
+        var imageDiv = $('#imageDiv');
+        var source = this.src;
 
-// Adds a mouseout event listner to return image to original size
-pics.mouseout( function () {
-    $(this).height(originalHeight).width(originalWidth);
-}); // END mouseout listener
+        var text = "";
+        text += "<img src=" + source + " height=400 width=600><br>";
+        text += "<span style:'position: relative; left: 260px; top: " +
+            "-390px;'> <a href='javascript:dowork2()'>Close</a></span> ";
 
-/*
- * Adds a click event listner to set the clicked image to the hidden div.
- * Notice the use of the 'this' keyword
- */
-pics.click( function (event) {
-    var imageDiv = $('#imageDiv');
-    var source = this.src;
+        imageDiv.html(text).css("visibility","visible");
+    }); // END listeners
 
-    var text = "";
-    text += "<img src=" + source + " height=400 width=600><br>";
-    text += "<span style:'position: relative; left: 260px; top: " +
-        "-390px;'> <a href='javascript:dowork2()'>Close</a></span> ";
-
-    imageDiv.html(text).css("visibility","visible");
-}); // END click listener
 
 
 function dowork2() {
     var imageDiv =$('#imageDiv');
-    imageDiv.html("<div></div>").css("visibility", "hidden");
+    imageDiv.html("<div></div>").css({"visibility": "hidden"});
 }
