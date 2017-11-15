145d5cc076600c6ed340ed29d5ad1edf33186342
kbriggs <kabriggs991@gmail.com>
2017-11-14 21:44:29 -0500

    countDown to jQuery

--- js/countDownJS.js
+++ js/countDownJS.js
@@ -1,13 +1,10 @@
-// Grab the elements from the HTML file
-var inputBox = document.getElementById('inputBox');
-var output = document.getElementById('output');
 
 // Set an event listener for keyup to adjust the output text
-inputBox.addEventListener('keyup', function () {
-    var charRemaining = 50 - inputBox.value.length;
-    output.innerHTML = charRemaining;
+$('#inputBox').keyup ( function () {
+    var charRemaining = 50 - $(this).val().length;
+    $('#output').text(charRemaining);
 
     if(charRemaining < 0) {
-        output.style.color = 'red';
+        $('#output').css('color','red');
     }
 });
